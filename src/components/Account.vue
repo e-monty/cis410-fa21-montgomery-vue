6615747841d80d9ae61dda69eaa85b7f3e5726f2
Eric <eric.montgomery@colostate.edu>
2021-12-07 11:19:21 -0700

    deployment prep, add assets

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,11 +1,39 @@
 <template>
   <div>
     <h1>Account</h1>
+    <hr />
+    <h3>{{ firstName }}'s Reviews</h3>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      reviewsByUser: null,
+      accountError: false,
+    };
+  },
+  computed: {
+    firstName() {
+      console.log("store so far", this.$store.state);
+      return this.$store.state.user.NameFirst;
+    },
+  },
+  created() {
+    axios
+      .get("/reviews/me", {
+        headers: {
+          Authorization: `Bearer ${this.$store.state.token}`,
+        },
+      })
+      .then((theResponse) => {
+        console.log("the response", theResponse);
+      });
+  },
+};
 </script>
 
 <style></style>
